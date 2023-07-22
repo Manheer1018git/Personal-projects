8db6baf0459efa91c59affb256d4d443985af5bc
Manheer1018git <139304224+Manheer1018git@users.noreply.github.com>
2023-07-22 12:29:29 +1000

    Add files via upload

---
+++ 000-code.cs
@@ -0,0 +1,214 @@
+﻿// we have used MealyMoore FSM in this task
+using System;
+namespace SimpleReactionMachine
+{
+    // (1) we declare an enum for the different states
+    enum State
+    {
+        Run,
+        Ready,
+        Wait,
+        Reaction,
+        Gamestop,
+        Results
+    }
+
+    // our class inherits the IController interface
+    public class SimpleReactionController : IController
+    {
+        // (2) we declare the state variable
+        private State _state;
+
+        // (3) we declare the gloabal variable and the constants
+        private const int GAME_TIME = 200;
+        private const int RESULT_TIME = 300;
+        private const int READY_TIME = 1000;
+        private const int RESULT_DISPLAY_TIME = 500;
+        private const double TICKS_IN_A_SECOND = 100.0;
+        private IGui _gui;
+        private IRandom _random;
+        private int _ticks;
+        private int _rndtime;
+        private int _games;
+        private double _reactiontime;
+        private double _reactiontime1;
+        private double _reactiontime2;
+        private double _reactiontime3;
+
+        // (4) we make a method to connect the interfaces IGui and IRandom and use it in a way of constructor
+        public void Connect(IGui gui, IRandom rng)
+        {
+            _gui = gui;
+            _random = rng;
+        }
+
+        // (5) Initialisation routine
+        public void Init()
+        {
+            Next(State.Run);
+        }
+
+        // (6) we make an event handler for when the user inserts coin
+        public void CoinInserted()
+        {
+            switch (_state)
+            {
+                case State.Run:
+                    Next(State.Ready);
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        // (7) we make an event handler for when user presses go/stop 
+        public void GoStopPressed()
+        {
+            switch (_state)
+            {
+                case State.Ready:
+                    Next(State.Wait);
+                    return;
+                case State.Wait:
+                    Next(State.Run);
+                    return;
+                case State.Reaction:
+                    _games++;
+                    Next(State.Gamestop);
+                    return;
+                case State.Gamestop:
+                    if (_games == 3)
+                    {
+                        Next(State.Results);
+                        return;
+                    }
+                    else
+                    {
+                        Next(State.Wait);
+                    }
+                    return;
+                case State.Results:
+                    Next(State.Run);
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        // (8) we make a method which handles the ticks which in our case represents 10 milliseconds
+        public void Tick()
+        {
+            switch (_state)
+            {
+                case State.Ready:
+                    _ticks++;
+                    if (_ticks == READY_TIME)
+                    {
+                        Next(State.Run);
+                    }
+                    return;
+                case State.Wait:
+                    _ticks++;
+                    if (_ticks == _rndtime)
+                    {
+                        Next(State.Reaction);
+                    }
+                    return;
+                case State.Reaction:
+                    _ticks++;
+                    _gui.SetDisplay((_ticks / TICKS_IN_A_SECOND).ToString("0.00"));
+                    if (_ticks == GAME_TIME)
+                    {
+                        if (_games == 0)
+                        {
+                            _reactiontime1 = (_ticks / TICKS_IN_A_SECOND);
+                        }
+                        else if (_games == 1)
+                        {
+                            _reactiontime2 = (_ticks / TICKS_IN_A_SECOND);
+                        }
+                        else if (_games == 2)
+                        {
+                            _reactiontime3 = (_ticks / TICKS_IN_A_SECOND);
+                        }
+                        _games++;
+                        Next(State.Gamestop);
+                    }
+                    if (_games == 0)
+                    {
+                        _reactiontime1 = (_ticks / TICKS_IN_A_SECOND);
+                    }
+                    else if (_games == 1)
+                    {
+                        _reactiontime2 = (_ticks / TICKS_IN_A_SECOND);
+                    }
+                    else if (_games == 2)
+                    {
+                        _reactiontime3 = (_ticks / TICKS_IN_A_SECOND);
+                    }
+                    return;
+                case State.Gamestop:
+                    _ticks++;
+                    if (_ticks == RESULT_TIME)
+                    {
+                        if (_games == 3)
+                        {
+                            Next(State.Results);
+                            return;
+                        }
+                        else
+                        {
+                            Next(State.Wait);
+                        }
+                    }
+                    return;
+                case State.Results:
+                    _ticks++;
+                    if (_ticks == RESULT_DISPLAY_TIME)
+                    {
+                        Next(State.Run);
+                    }
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        // (9) we make a method which handles actions on entry to a state
+        void Next(State state)
+        {
+            _state = state;
+            _ticks = 0;
+
+            switch (_state)
+            {
+                case State.Run:
+                    _games = 0;
+                    _reactiontime = 0;
+                    _reactiontime1 = 0;
+                    _reactiontime2 = 0;
+                    _reactiontime3 = 0;
+                    _rndtime = 0;
+                    _gui.SetDisplay("Insert coin");
+                    return;
+                case State.Ready:
+                    _gui.SetDisplay("Press GO!");
+                    return;
+                case State.Wait:
+                    _rndtime = _random.GetRandom(100, 250);
+                    _gui.SetDisplay("Wait...");
+                    return;
+                case State.Reaction:
+                    _gui.SetDisplay("0.00");
+                    return;
+                case State.Results:
+                    _reactiontime = _reactiontime1 + _reactiontime2 + _reactiontime3;
+                    _gui.SetDisplay("Average reaction time: " + (_reactiontime / _games).ToString("0.00"));
+                    _ticks = 0;
+                    return;
+                default:
+                    return;
+            }
+        }
+    }
+}
---
+++ 001-code.cs
@@ -0,0 +1,350 @@
+﻿using System;
+
+namespace SimpleReactionMachine
+{
+    class Tester
+    {
+        private static IController controller;
+        private static IGui gui;
+        private static string displayText;
+        private static int randomNumber;
+        private static int passed = 0;
+
+        static void Main(string[] args)
+        {
+            // run simple test
+            SimpleTest();
+            Console.WriteLine("\n=====================================\nSummary: {0} tests passed out of 76", passed);
+            Console.ReadKey();
+        }
+
+        private static void SimpleTest()
+        {
+            //Construct a ReactionController
+            controller = new SimpleReactionController();
+            gui = new DummyGui();
+
+            //Connect them to each other
+            gui.Connect(controller);
+            controller.Connect(gui, new RndGenerator());
+
+            //Reset the components()
+            gui.Init();
+
+            // *************************************************************
+            // PART 1 - NORMAL GAME
+            //Test the SimpleReactionController
+            //IDLE
+            DoReset('A', controller, "Insert coin");
+            DoGoStop('B', controller, "Insert coin");
+            DoTicks('C', controller, 1, "Insert coin");
+
+            //coinInserted
+            DoInsertCoin('D', controller, "Press GO!");
+
+            //READY
+            DoTicks('E', controller, 1, "Press GO!");
+            DoInsertCoin('F', controller, "Press GO!");
+
+            //goStop
+            randomNumber = 117;
+            DoGoStop('G', controller, "Wait...");
+
+            //WAIT tick(s)
+            DoTicks('H', controller, randomNumber - 1, "Wait...");
+
+            //RUN tick(s)
+            DoTicks('I', controller, 1, "0.00");
+            DoTicks('J', controller, 1, "0.01");
+            DoTicks('K', controller, 11, "0.12");
+            DoTicks('L', controller, 88, "1.00");
+
+            //goStop
+            DoGoStop('M', controller, "1.00");
+
+            //STOP tick(s)
+            DoTicks('N', controller, 299, "1.00");
+
+            // game-2
+            //tick
+            randomNumber = 170;
+            DoGoStop('O', controller, "Wait...");
+
+            //WAIT tick(s)
+            DoTicks('P', controller, randomNumber - 1, "Wait...");
+
+            //RUN tick(s)
+            DoTicks('Q', controller, 1, "0.00");
+            DoTicks('R', controller, 100, "1.00");
+
+            //goStop
+            DoGoStop('S', controller, "1.00");
+
+            //STOP tick(s)
+            DoTicks('T', controller, 299, "1.00");
+
+            // game-3
+            //tick
+            randomNumber = 220;
+            DoGoStop('U', controller, "Wait...");
+
+            //WAIT tick(s)
+            DoTicks('V', controller, randomNumber - 1, "Wait...");
+
+            //RUN tick(s)
+            DoTicks('W', controller, 1, "0.00");
+            DoTicks('X', controller, 100, "1.00");
+
+            //goStop
+            DoGoStop('Y', controller, "1.00");
+
+            //STOP tick(s)
+            DoTicks('Z', controller, 299, "1.00");
+
+            //Reaction time displayed
+            DoGoStop('a', controller, "Average reaction time: 1.00");
+            DoTicks('b', controller, 499, "Average reaction time: 1.00");
+
+            // new game ready to be started
+            DoTicks('c', controller, 1, "Insert coin");
+
+            // *************************************************************
+            // PART 2 - 10 SECONDS AFTER COIN INSERTION
+            //IDLE init
+            gui.Init();
+            DoReset('d', controller, "Insert coin");
+
+            //coinInserted
+            DoInsertCoin('e', controller, "Press GO!");
+
+            //READY
+            DoTicks('f', controller, 1, "Press GO!");
+            DoTicks('g', controller, 998, "Press GO!");
+
+            //TIMEOUT
+            DoTicks('h', controller, 999, "Insert coin");
+
+            // *************************************************************
+            // PART 3 - WAIT: GOSTOP DURING GAME 1
+            //IDLE init
+            gui.Init();
+            DoReset('i', controller, "Insert coin");
+
+            //coinInserted
+            DoInsertCoin('j', controller, "Press GO!");
+
+            //READY
+            DoTicks('k', controller, 1, "Press GO!");
+
+            //goStop
+            randomNumber = 117;
+            DoGoStop('l', controller, "Wait...");
+            DoTicks('f', controller, 50, "Wait...");
+
+            //CHEATING - RESET
+            DoGoStop('n', controller, "Insert coin");
+
+            // *************************************************************
+            // PART 4 - WAIT: GOSTOP DURING GAME 3
+            //IDLE init
+            gui.Init();
+            DoReset('o', controller, "Insert coin");
+
+            //coinInserted
+            DoInsertCoin('p', controller, "Press GO!");
+
+            //READY
+            DoTicks('q', controller, 1, "Press GO!");
+
+            //goStop
+            randomNumber = 117;
+            DoGoStop('r', controller, "Wait...");
+
+            //WAIT tick(s)
+            DoTicks('s', controller, randomNumber - 1, "Wait...");
+
+            //RUN tick(s)
+            DoTicks('t', controller, 1, "0.00");
+            DoTicks('u', controller, 100, "1.00");
+
+            //goStop
+            DoGoStop('v', controller, "1.00");
+
+            //STOP tick(s)
+            DoTicks('w', controller, 299, "1.00");
+
+            // game-2
+            //tick
+            randomNumber = 170;
+            DoGoStop('x', controller, "Wait...");
+
+            //WAIT tick(s)
+            DoTicks('y', controller, randomNumber - 1, "Wait...");
+
+            //RUN tick(s)
+            DoTicks('z', controller, 1, "0.00");
+            DoTicks('1', controller, 100, "1.00");
+
+            //goStop
+            DoGoStop('2', controller, "1.00");
+
+            //STOP tick(s)
+            DoTicks('3', controller, 299, "1.00");
+
+            // game-3
+            //tick
+            randomNumber = 220;
+            DoGoStop('4', controller, "Wait...");
+
+            //CHEATING - RESET
+            DoGoStop('5', controller, "Insert coin");
+
+            // *************************************************************
+            // PART 5 - IDLE -> READY init
+            //IDLE init
+            gui.Init();
+            DoReset('6', controller, "Insert coin");
+
+            randomNumber = 123;
+            DoInsertCoin('7', controller, "Press GO!");
+            // *********************************new game?	
+            gui.Init();
+            DoReset('8', controller, "Insert coin");
+
+            // *************************************************************
+            // PART 6 - IDLE -> READY -> WAIT init
+            randomNumber = 123;
+            DoInsertCoin('9', controller, "Press GO!");
+            DoGoStop('A', controller, "Wait...");
+            // *********************************new game?
+            gui.Init();
+            DoReset('B', controller, "Insert coin");
+
+            // *************************************************************
+            // PART 7 - IDLE -> READY -> WAIT -> RUN init
+            randomNumber = 137;
+            DoInsertCoin('C', controller, "Press GO!");
+            DoGoStop('D', controller, "Wait...");
+            DoTicks('E', controller, randomNumber + 98, "0.98");
+            // *********************************new game?
+            gui.Init();
+            DoReset('F', controller, "Insert coin");
+
+            // *************************************************************
+            // PART 8 - IDLE -> READY -> WAIT -> RUN -> STOP init
+            randomNumber = 119;
+            DoInsertCoin('G', controller, "Press GO!");
+            DoGoStop('H', controller, "Wait...");
+            DoTicks('I', controller, randomNumber + 135, "1.35");
+            DoGoStop('J', controller, "1.35");
+            // *********************************new game?
+            gui.Init();
+            DoReset('K', controller, "Insert coin");
+
+            // *************************************************************
+            // PART 9 - IDLE -> READY -> WAIT -> RUN (timeout) -> STOP
+            randomNumber = 120;
+            DoInsertCoin('L', controller, "Press GO!");
+            DoGoStop('M', controller, "Wait...");
+            DoTicks('N', controller, randomNumber + 199, "1.99");
+            DoTicks('O', controller, 50, "2.00");
+
+            // *************************************************************            
+        }
+
+        private static void DoReset(char ch, IController controller, string msg)
+        {
+            try
+            {
+                controller.Init();
+                GetMessage(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void DoGoStop(char ch, IController controller, string msg)
+        {
+            try
+            {
+                controller.GoStopPressed();
+                GetMessage(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void DoInsertCoin(char ch, IController controller, string msg)
+        {
+            try
+            {
+                controller.CoinInserted();
+                GetMessage(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void DoTicks(char ch, IController controller, int n, string msg)
+        {
+            try
+            {
+                for (int t = 0; t < n; t++) controller.Tick();
+                GetMessage(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void GetMessage(char ch, string msg)
+        {
+            if (msg.ToLower() == displayText.ToLower())
+            {
+                Console.WriteLine("test {0}: passed successfully", ch);
+                passed++;
+            }
+            else
+                Console.WriteLine("test {0}: failed with message ( expected {1} | received {2})", ch, msg, displayText);
+        }
+
+        private class DummyGui : IGui
+        {
+
+            private IController controller;
+
+            public void Connect(IController controller)
+            {
+                this.controller = controller;
+            }
+
+            public void Init()
+            {
+                displayText = "?reset?";
+            }
+
+            public void SetDisplay(string msg)
+            {
+                displayText = msg;
+            }
+        }
+
+        private class RndGenerator : IRandom
+        {
+            public int GetRandom(int from, int to)
+            {
+                return randomNumber;
+            }
+        }
+
+    }
+
+}
+
