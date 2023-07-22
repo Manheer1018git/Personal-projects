bdf82d439bd78f2cd65adc80643881f74a8cc571
Manheer1018git <139304224+Manheer1018git@users.noreply.github.com>
2023-07-22 12:38:08 +1000

    Add files via upload

---
+++ MinDequesNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MinDequesNumber
+{
+    public class MinDequesNumber
+    {
+
+        public static int Solve(int[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
